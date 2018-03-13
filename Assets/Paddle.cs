c91018e82862d21eaacb55b28a5ab847dee49ddc
Manuel <wtfjoke@livenet.ch>
2018-03-13 13:01:32 +0100

    first lecture

---
+++ Assets/Paddle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    public Vector3 speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float direction = Input.GetAxis("Horizontal");
+        transform.position = transform.position + direction * speed * Time.deltaTime;
+	}
+}
