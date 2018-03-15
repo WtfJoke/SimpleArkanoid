e57d2cb6a567f4636d808928e66acc574d1a569e
Manuel <wtfjoke@livenet.ch>
2018-03-15 12:42:36 +0100

    second lecture - add basic stuff (collision and other stuff)

---
+++ Assets/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public float zSpeed;
+	public float xMax;
+	private Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+		velocity = new Vector3 (0, 0, zSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += velocity * Time.deltaTime;
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Paddle") {
+			
+			float maxDistance = transform.localScale.x * 0.5f + other.transform.localScale.x * 0.5f;
+			float actualDistance = transform.position.x - other.transform.position.x;
+			float normalizedDistance = actualDistance / maxDistance;
+
+			velocity = new Vector3 (normalizedDistance * xMax, velocity.y, -velocity.z);
+		}
+		else if (other.tag == "Wall") {
+			velocity = new Vector3 (-velocity.x, velocity.y, velocity.z);
+		}
+		gameObject.GetComponent<AudioSource> ().Play ();
+	}
+}
--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Paddle : MonoBehaviour {
 
     public Vector3 speed;
+	public Transform floor;
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +15,12 @@ public class Paddle : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         float direction = Input.GetAxis("Horizontal");
-        transform.position = transform.position + direction * speed * Time.deltaTime;
+		float newPos = transform.position.x + direction * Time.deltaTime * speed.x;
+		float paddleScale = transform.localScale.x;
+		float floorScale = floor.localScale.x;
+		float maxPos = floorScale * 10 * 0.5f - paddleScale * 1 * 0.5f;
+
+		float newPositionWithRangeCheck = Mathf.Clamp (newPos, -maxPos, maxPos);
+		transform.position = new Vector3(newPositionWithRangeCheck, transform.position.y, transform.position.z);
 	}
 }
