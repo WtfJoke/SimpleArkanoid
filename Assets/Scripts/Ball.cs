9e77189c4668bdcd4d505a635b74c31979dce750
Manuel <wtfjoke@livenet.ch>
2018-03-20 12:56:38 +0100

    restructure assets and add basic arkanoid principle

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public float zSpeed;
+	public float xMax;
+    public Vector3 respawnPosition = new Vector3(0,0.5f,0);
+	private Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+		velocity = new Vector3 (0, 0, zSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += velocity * Time.deltaTime;
+	}
+
+	void OnTriggerEnter(Collider other){
+        switch (other.tag)
+        {
+            case "Player":
+                velocity = GetNormalicedVelocity(other);
+                gameObject.GetComponent<AudioSource>().Play();
+                break;
+            case "Brick":
+                velocity = GetNormalicedVelocity(other);
+                Destroy(other.gameObject);
+                GameObject.FindObjectOfType<GameManager>().ReduceBrick();
+                // TODO: add point
+                break;
+            case "Wall":
+                velocity = new Vector3(-velocity.x, velocity.y, velocity.z);
+                break;
+            case "HorizontalWall":
+                velocity = new Vector3(velocity.x, velocity.y, -velocity.z);
+                break;
+            case "LostBallWall":
+                // TODO: reduce life
+                GameObject.FindGameObjectWithTag("Ball").transform.position = respawnPosition;
+                break;
+        }
+	}
+
+    private Vector3 GetNormalicedVelocity(Collider other)
+    {
+        float maxDistance = transform.localScale.x * 0.5f + other.transform.localScale.x * 0.5f;
+        float actualDistance = transform.position.x - other.transform.position.x;
+        float normalizedDistance = actualDistance / maxDistance;
+
+        return new Vector3(normalizedDistance * xMax, velocity.y, -velocity.z);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public int remainingBricks;
+
+	// Use this for initialization
+	void Start () {
+        remainingBricks = GameObject.FindGameObjectsWithTag("Brick").Length;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		// TODO restart game on remainingBricks == 0
+	}
+
+    public void ReduceBrick()
+    {
+        remainingBricks -= 1;
+    }
+}
