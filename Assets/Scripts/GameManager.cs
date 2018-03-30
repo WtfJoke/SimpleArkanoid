2fa0f1fd7c106a8c733b138ee2d2c139ab997fa4
Manuel <wtfjoke@livenet.ch>
2018-03-30 19:27:10 +0200

    implement gameover/won screen and restart

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,10 @@ public class GameManager : MonoBehaviour {
     public int remainingBricks;
     public Text pointsText;
     public Text lifeText;
+    public Text gameOverText;
+    public Text youWonText;
+    public Text restartText;
+    public float finishTextDisplayTime = 0.1f;
     public int Points
     {
         get { return _points; }
@@ -32,14 +36,16 @@ public class GameManager : MonoBehaviour {
             lifeText.text = hearts;
             if (_lifes == 0)
             {
-                RestartGame();
+                GameOver();
             }
         }
     }
+
+    private bool isRunning;
     private int _points;
     private int _lifes;
     private GameObject[] bricks;
-
+    
     void Awake()
     {
         if (instance == null)
@@ -52,15 +58,37 @@ public class GameManager : MonoBehaviour {
         }
         InitGame();
     }
-    
+
+    public void Update()
+    {
+        if (!isRunning && Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartGame();   
+        }
+    }
+
 
     public void ReduceBrick()
     {
         remainingBricks -= 1;
         if (remainingBricks == 0)
         {
-            RestartGame();
+            GameOver();
         }
+    }
+
+    private void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartText.gameObject.SetActive(true);
+        isRunning = false;
+    }
+
+    private void Won()
+    {
+        youWonText.gameObject.SetActive(true);
+        restartText.gameObject.SetActive(true);
+        isRunning = false;
     }
 
     private void InitGame()
@@ -71,6 +99,11 @@ public class GameManager : MonoBehaviour {
 
     private void StartGame()
     {
+        isRunning = true;
+        youWonText.gameObject.SetActive(false);
+        gameOverText.gameObject.SetActive(false);
+        restartText.gameObject.SetActive(false);
+
         foreach (var brickObj in bricks)
         {
             Brick brick = brickObj.GetComponent<Brick>();
