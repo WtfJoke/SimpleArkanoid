66345c8fee7ad54bd07e70cedf0323c914b59dc8
Manuel <wtfjoke@livenet.ch>
2018-03-31 14:08:08 +0200

    destroy power up when in deadzone

--- Assets/PowerUp.cs
+++ Assets/PowerUp.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUp : MonoBehaviour {
+public class PowerUp : MonoBehaviour
+{
 
     public float Speed = 3f;
-    public PowerUpType type; 
-    public enum PowerUpType { Enlarge, Shrink, TwoBalls};
-	
-	void Update () {
+    public PowerUpType type;
+    public enum PowerUpType { Enlarge, Shrink, TwoBalls };
+
+    void Update()
+    {
         float newPos = transform.position.z + Time.deltaTime * -Speed;
         transform.position = new Vector3(transform.position.x, transform.position.y, newPos);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("LostBallWall"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
